48af93b6af81d38a2fc82182da97ea79601aa190
marcost2 <marcostolcachir@gmail.com>
2021-10-14 20:18:53 -0300

    Set up CORSMiddleware
    * Set up the Cross-Origin Resource Sharing so as to allow frontend's running from any origin to access the backend
    For now during testing this is acceptable but for deployment a hostlist will need to be made
    
    * This finally allows the backend to answer the CORS OPTIONS request from the frontend, allowing it to send requests successfully

--- Misterio/server.py
+++ Misterio/server.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 # Standart html file to render on server startup
 htmlfilepath = 'test.html'
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def get():
