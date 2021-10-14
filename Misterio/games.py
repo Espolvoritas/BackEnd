38dc1c45aef5cc377fbf0b6e6375d0e500734359
marcost2 <marcostolcachir@gmail.com>
2021-10-13 22:48:33 -0300

    [TMP] Create games router and skeleton for CreateNew endpoint
    * Uses a list as placeholder until the db is ready

---
+++ Misterio/games.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, HTTPException, status
+
+game = APIRouter(prefix="/game")
+
+games = []
+
+class Game:
+	def __init__(self, id, name, host):
+	  self.id = id
+	  self.name = name
+	  self.host = host
+	  self.players = [host]
+
+@game.post("/createNew", status_code=status.HTTP_201_CREATED)
+async def createNewGame(name: str, host: str) -> int:
+	for x in games:
+		if x.name == name:
+			raise HTTPException(status_code=400, detail="The game name is already in use")
+	ID = len(games)+1
+	new_game = Game(ID ,name, host)
+	games.append(new_game)
+	return ID
