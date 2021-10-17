657d12f071a46c6240f9f2f0a51945b37156f23b
marcost2 <marcostolcachir@gmail.com>
2021-10-17 19:28:40 -0300

    Add createNew unittest
    * Run by calling pytest

---
+++ Misterio/test_createGames.py
@@ -0,0 +1,71 @@
+from server import app
+from fastapi.testclient import TestClient
+import string    
+import random # define the random module  
+
+client = TestClient(app)
+
+#aux function for getting random strings
+def get_random_string(length):
+    # choose from all lowercase letter
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+def test_create_new_game():
+    response = client.post("/game/createNew",
+		headers={"accept": "application/json",
+				"Content-Type" : "application/json"},
+				json={"name": get_random_string(6), "host": get_random_string(6)}
+				)
+    assert response.status_code == 201
+    assert type(response.json()) is int
+
+def test_create_new_game_missing_name():
+    response = client.post("/game/createNew",
+		headers={"accept": "application/json",
+				"Content-Type" : "application/json"},
+				json={"host": get_random_string(6)}
+				)
+    assert response.status_code == 422
+    assert response.json() == {'detail': [{'loc': ['body', 'name'], 'msg': 'field required', 
+     'type': 'value_error.missing'}]}
+
+def test_create_new_game_missing_host():
+    response = client.post("/game/createNew",
+		headers={"accept": "application/json",
+				"Content-Type" : "application/json"},
+				json={"name": get_random_string(6)}
+				)
+    assert response.status_code == 422
+    assert response.json() == {'detail': [{'loc': ['body', 'host'], 'msg': 'field required', 
+     'type': 'value_error.missing'}]}
+
+def test_create_new_game_missing_all():
+    response = client.post("/game/createNew",
+		headers={"accept": "application/json",
+				"Content-Type" : "application/json"},
+				json={}
+				)
+    assert response.status_code == 422
+    assert response.json() == {'detail': [{'loc': ['body', 'name'], 'msg': 'field required',
+     'type': 'value_error.missing'}, {'loc': ['body', 'host'], 'msg': 'field required', 'type': 
+     'value_error.missing'}]}
+
+def test_create_new_game_repeated_name():
+    reuse_name = get_random_string(6)
+    response = client.post("/game/createNew",
+		headers={"accept": "application/json",
+				"Content-Type" : "application/json"},
+				json={"name": reuse_name, "host": get_random_string(6)}
+				)
+    assert response.status_code == 201
+    assert type(response.json()) is int
+    #Now do the request with the same game name
+    response = client.post("/game/createNew",
+		headers={"accept": "application/json",
+				"Content-Type" : "application/json"},
+				json={"name": reuse_name, "host": get_random_string(6)}
+				)
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'The game name is already in use'}
