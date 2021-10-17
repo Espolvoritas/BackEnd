41aaa1060a5cf2f18e7100c9829ecf0fe2387d45
marcost2 <marcostolcachir@gmail.com>
2021-10-17 19:28:17 -0300

    Adjust createNew to use the DB
    * Support has landed and it works wonderfully, so switch to using it

--- Misterio/games.py
+++ Misterio/games.py
@@ -1,22 +1,16 @@
 from fastapi import APIRouter, HTTPException, status, Body
+import database as db
+from pony.orm import db_session, flush
 
 game = APIRouter(prefix="/game")
 
-games = []
-
-class Game:
-	def __init__(self, id, name, host):
-	  self.id = id
-	  self.name = name
-	  self.host = host
-	  self.players = [host]
-
 @game.post("/createNew", status_code=status.HTTP_201_CREATED)
 async def createNewGame(name: str = Body(...), host: str = Body(...)) -> int:
-	for x in games:
-		if x.name == name:
+	with db_session:
+		if db.Game.get(name=name) is not None:
 			raise HTTPException(status_code=400, detail="The game name is already in use")
-	ID = len(games)+1
-	new_game = Game(ID ,name, host)
-	games.append(new_game)
-	return ID
+		new_player = db.Player(nickName=host)
+		new_game = db.Game(name=name, host=new_player, isStarted=False)
+		flush()
+		new_game.addPlayer(new_player)
+		return new_game.game_id
