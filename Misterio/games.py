0828ffdb832f6f166823d4f59c7a8c15922ea02e
Valenteam24 <valen.arrascaeta@gmail.com>
2021-10-18 17:54:14 -0300

    backend endpoint for seeing available games

--- Misterio/games.py
+++ Misterio/games.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException, status, Body
+from starlette.responses import Response
 import database as db
-from pony.orm import db_session, flush
+from pony.orm import db_session, flush, select
 
 game = APIRouter(prefix="/game")
 
@@ -14,3 +15,21 @@ async def createNewGame(name: str = Body(...), host: str = Body(...)):
 		flush()
 		new_game.addPlayer(new_player)
 		return {"game_id": new_game.game_id, "player_id": new_player.player_id}
+
+
+@game.get("/availableGames", status_code=status.HTTP_200_OK)
+async def getAvailableGames(response: Response):
+	with db_session:
+		games_query = select(g for g in db.Game if (g.playerCount < 6)).order_by(db.Game.game_id)
+		if not games_query.exists():
+			response.status_code = status.HTTP_204_NO_CONTENT	
+		gamelist = []
+		totalGames = len(games_query)
+		for g in games_query:
+			game = {}
+			game['name'] = g.name
+			game['players'] = int(g.playerCount)
+			game['host'] = g.host.nickName
+			game['password'] = False #We dont have passwords yet
+			gamelist.append(game)
+	return {'totalGames':totalGames, 'games':gamelist}
