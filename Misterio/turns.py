1a2cbb8e47d23a8e2afde8546ad1d3f189375b25
Valenteam24 <valen.arrascaeta@gmail.com>
2021-10-23 18:30:20 -0300

    start game should go in other branch

--- Misterio/turns.py
+++ Misterio/turns.py
@@ -6,20 +6,6 @@ gameBoard = APIRouter(prefix="/gameBoard")
 
 gameBoard_manager = ConnectionManager()
 
-@gameBoard.websocket("/game/startGame")
-async def startGame(websocket: WebSocket, gameID: int):
-	with db_session:
-		game = db.Game.get(game_id=gameID)
-		if game is None:
-			await gameBoard_manager.send_personal_message(status.HTTP_400_BAD_REQUEST,websocket)
-			await websocket.close()
-			return
-		if game.playerCount < 2:
-			await gameBoard_manager.send_personal_message(status.HTTP_405_METHOD_NOT_ALLOWED,websocket)
-		else:
-			game.isStarted = True
-			game.sortPlayers()
-
 @gameBoard.websocket("/gameBoard/{userID}/rollDice")
 async def rollDice(websocket: WebSocket, userID: int):
 	await gameBoard_manager.connect(websocket, userID)
