bad7f1ba5b6d095963cb2945f6548858c5ddf20e
alvinary <alvaro.cuestas.ll@gmail.com>
2021-10-23 20:38:19 -0300

    Update joinGame to work with frontend
    Now exceptional cases are managed via HTPP status codes

--- Misterio/games.py
+++ Misterio/games.py
@@ -70,32 +70,32 @@ manager = ConnectionManager()
 
 @game.post("/createNew", status_code=status.HTTP_201_CREATED)
 async def createNewGame(name: str = Body(...), host: str = Body(...)):
-	with db_session:
-		if db.Game.get(name=name) is not None:
-			raise HTTPException(status_code=400, detail="The game name is already in use")
-		new_player = db.Player(nickName=host)
-		new_game = db.Game(name=name, host=new_player, isStarted=False)
-		flush()
-		new_game.addPlayer(new_player)
-		return {"game_id": new_game.game_id, "player_id": new_player.player_id}
+    with db_session:
+        if db.Game.get(name=name) is not None:
+            raise HTTPException(status_code=400, detail="The game name is already in use")
+        new_player = db.Player(nickName=host)
+        new_game = db.Game(name=name, host=new_player, isStarted=False)
+        flush()
+        new_game.addPlayer(new_player)
+        return {"game_id": new_game.game_id, "player_id": new_player.player_id}
 
 @game.get("/availableGames", status_code=status.HTTP_200_OK)
 async def getAvailableGames():
-	gamelist = []
-	with db_session:
-		games_query = select(g for g in db.Game if (g.playerCount < 6)).order_by(db.Game.name)	
-		for g in games_query:
-			game = {}
-			game["name"] = g.name
-			game["id"] = g.game_id
-			game["players"] = int(g.playerCount)
-			game["host"] = g.host.nickName
-			game["password"] = False #We dont have passwords yet
-			gamelist.append(game)
-	if not gamelist:
-		return Response(status_code=status.HTTP_204_NO_CONTENT)
-		
-	return gamelist
+    gamelist = []
+    with db_session:
+        games_query = select(g for g in db.Game if (g.playerCount < 6)).order_by(db.Game.name)    
+        for g in games_query:
+            game = {}
+            game["name"] = g.name
+            game["id"] = g.game_id
+            game["players"] = int(g.playerCount)
+            game["host"] = g.host.nickName
+            game["password"] = False #We dont have passwords yet
+            gamelist.append(game)
+    if not gamelist:
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+        
+    return gamelist
 
 @game.websocket("/game/getPlayers/{userID}")
 async def getPlayers(websocket: WebSocket, userID: int):
@@ -156,27 +156,28 @@ class JoinGameResponse(BaseModel):
     playerId: int
 
 @game.post("/joinCheck")
-def joinGame(gameId: int = Body(...), playerNickname: str = Body(...)):
-    
+@game.post("/joinCheck")
+async def joinGame(gameId: int = Body(...), playerNickname: str = Body(...)):
+
     with db_session:
-        
+
         chosenGame = db.Game.get(game_id=gameId)
-        
+
         if chosenGame is None:
-            return { "nicknameIsValid": False, "playerId": -1, "gameIdIsValid": False }
-        
+            raise HTTPException(status_code=404, detail="Bad Request")
+
         existingNicknames = set([player.nickName for player in select(p for p in chosenGame.players)])
         nicknameIsTaken = playerNickname in existingNicknames
-        
+
         if nicknameIsTaken:
-            return { "nicknameIsValid": False, "playerId": -1, "gameIdIsValid": True }
-        
+            return { "nicknameIsValid": False, "playerId": -1, "gameIdIsValid": False }
+
         if chosenGame is not None and not nicknameIsTaken:
-            
+
             newPlayer = db.Player(nickName=str(playerNickname))
-            
+
             flush() # flush so the newPlayer is committed to the database
-            
+
             chosenGame.addPlayer(newPlayer)
             newPlayerId = newPlayer.player_id
 
