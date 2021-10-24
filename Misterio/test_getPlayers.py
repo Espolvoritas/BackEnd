e9833df17e20441239dade03510b966cf6b4e217
marcost2 <marcostolcachir@gmail.com>
2021-10-24 17:50:44 -0300

    Fix another misnamed test

--- Misterio/test_getPlayers.py
+++ Misterio/test_getPlayers.py
@@ -98,7 +98,7 @@ def test_two_lobbys():
 			websocket2.close()
 			websocket1.close()
 
-def test_no_lobby():
+def test_duplicate():
 	clear_tables()
 	host = get_random_string(6)
 	create_new_game(host)
