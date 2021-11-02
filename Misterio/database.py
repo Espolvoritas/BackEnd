3d710124192f37eca7f3f063da4e6fddfb2f4347
Valenteam24 <valen.arrascaeta@gmail.com>
2021-11-02 15:35:33 -0300

    Create function ro retrieve available colors in lobby

--- Misterio/database.py
+++ Misterio/database.py
@@ -46,6 +46,12 @@ class Game(db.Entity):
             player.nextPlayer = shuffledPlayers[(index + 1) % len(shuffledPlayers)]
         self.currentPlayer = shuffledPlayers[0]
 
+    def getAvailableColors(self):
+        #Returns a list of type Color elements that have not been assigned
+        player_colors = (select (p.color for p in db.Player if p.lobby.game_id == self.game_id))
+        colors = list(select(c for c in db.Color if c not in player_colors))
+        return colors
+
 class Player(db.Entity):
     player_id = PrimaryKey(int, auto=True) 
     nickName = Required(str)
