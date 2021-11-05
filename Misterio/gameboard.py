fef0b8041ed451f684117d66e9b84738c23d485e
Valenteam24 <valen.arrascaeta@gmail.com>
2021-11-05 16:27:05 -0300

    Add status codes to messages throug websockets

--- Misterio/gameboard.py
+++ Misterio/gameboard.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, status, WebSocket, WebSocketDisconnect
-from pony.orm import db_session
+from fastapi import APIRouter, status, WebSocket, WebSocketDisconnect, HTTPException
+from pony.orm import db_session, select
 
 import Misterio.database as db
 from Misterio.constants import *
@@ -109,16 +109,27 @@ async def handleTurn(websocket: WebSocket, userID: int):
 	with db_session:
 		player = db.Player.get(player_id=userID)
 		lobby = player.lobby
-		await gameBoard_manager.send_personal_message({"code" : WS_CURR_PLAYER + WS_CARD_LIST ,
-		"currentPlayer" : lobby.currentPlayer.nickName, "cards" : get_card_list(userID)}, websocket)
+		responseMessage = {'status':'PLAYERINTURN', 'args':[lobby.currentPlayer.nickName]}
+		await gameBoard_manager.send_personal_message(responseMessage, websocket)
 	try:
 		while(True):
-			roll = await websocket.receive_text()
-			if player_in_turn(userID):
-				with db_session:
-					player = db.Player.get(player_id=userID)
-					player.currentDiceRoll = int(roll)
-				await gameBoard_manager.lobby_broadcast({"code" : WS_CURR_PLAYER, "currentPlayer" : get_next_turn(lobby.game_id)}, lobby.game_id)
+			message = await websocket.receive_json()
+
+			if message['status'] == 'SUSPICION':
+				suspicion = message['args']
+				suspicionResponse = await check_suspicion(userID, suspicion)
+				await gameBoard_manager.send_personal_message(suspicionResponse, websocket)
+				responseMessage = {'status':'PLAYERINTURN', 'args':[get_next_turn(lobby.game_id)]}
+				await gameBoard_manager.lobby_broadcast(responseMessage, lobby.game_id)
+
+			if message['status'] == 'DICEROLL':
+				roll = message['args'].pop()
+				if player_in_turn(userID):
+					with db_session:
+						player = db.Player.get(player_id=userID)
+						player.currentDiceRoll = int(roll)
+				responseMessage = {'status':'PLAYERINTURN', 'args':[get_next_turn(lobby.game_id)]}
+				await gameBoard_manager.lobby_broadcast(responseMessage, lobby.game_id)
 	except WebSocketDisconnect:
 		gameBoard_manager.disconnect(websocket, lobby.game_id)
 		await gameBoard_manager.lobby_broadcast(await gameBoard_manager.getPlayers(lobby.game_id), lobby.game_id)
