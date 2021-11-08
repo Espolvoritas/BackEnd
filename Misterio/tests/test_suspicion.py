8f9aca6d8f3588cc4b93e871009e0f379de19d9e
Valenteam24 <valen.arrascaeta@gmail.com>
2021-11-07 22:37:04 -0300

    fix tests and add test for 2 player suspicion

--- Misterio/tests/test_suspicion.py
+++ Misterio/tests/test_suspicion.py
@@ -16,6 +16,90 @@ client = TestClient(app)
 #test if suspicion is completely in envelope
 #test if suspicion doesnt require picking
 
+def test_makeSuspicion_2players():
+	db.clear_tables()
+	host = get_random_string(6)
+	game_id = create_game_post(host, client).json()['game_id']
+	expectedPlayers = create_players(1, game_id)
+	expectedPlayers.insert(0,host)
+	
+	with client.websocket_connect("/lobby/1") as websocket1, \
+		client.websocket_connect("/lobby/2") as websocket2:
+		try:
+			data = websocket1.receive_json()
+			for player, expected in zip(data['players'], expectedPlayers):
+				assert (player['nickName'] == expected)
+			data = websocket2.receive_json()
+			for player, expected in zip(data['players'], expectedPlayers):
+				assert (player['nickName'] == expected)
+			
+			response = startGame_post(1, client)
+			assert (response.status_code == 200)
+			
+			websocket2.close()
+			data = websocket1.receive_json()
+			for player, expected in zip(data['players'], expectedPlayers):
+				assert (player['nickName'] == expected)
+			websocket1.close()
+		except KeyboardInterrupt:
+			websocket2.close()
+			websocket1.close()
+	
+	with db_session:
+		player1 = db.Game.get(game_id=game_id).currentPlayer
+		player2 = player1.nextPlayer
+		player1.inRoom = True
+		
+		roomCards = select(c for c in player2.cards if c.cardType == 'Room')
+		roomCard = roomCards.first()
+		room1 = db.Cell()
+		room1.roomName = roomCard.cardName
+		player1.location = room1
+		cards1 = list(player1.cards)
+		cards2 = list(player2.cards)
+		cards3 = list(player2.cards)
+		roomcard = db.Card.get(cardName=room1.roomName)
+		culprit = select(c for c in db.Card if c in player2.cards and c.cardType == 'Monster').first()
+		victim = select(c for c in db.Card if c in player2.cards and c.cardType == 'Victim').first()
+		print(culprit.cardName)
+		print(victim.cardName)
+		culprit = culprit.cardId
+		victim = victim.cardId
+	with client.websocket_connect("/gameBoard/" + str(player1.player_id)) as websocket1:
+		currplayer = websocket1.receive_json()
+		with client.websocket_connect("/gameBoard/" + str(player2.player_id)) as websocket2:
+			currplayer = websocket2.receive_json()#connection broadcast
+			try:
+				print("Player 1 Cards: ", cards1)
+				print("Player 2 Cards: ", cards2)
+				print("Suspicion: ", victim, culprit, roomCard.cardId )
+				websocket2.send_json({'status': 'PICK_CARD', 'card': victim})
+				response = suspicion_post(player1.player_id, victim, culprit, client)
+				data1 = websocket1.receive_json()
+				data2 = websocket2.receive_json()
+				
+				print(data1)
+				print(data2)
+				data2 = websocket2.receive_json()
+				print(data2)
+				
+				data1 = websocket1.receive_json()
+				data2 = websocket2.receive_json()
+				print(data1)
+				print(data2)
+
+				assert (response.status_code == 200)
+				print(response)	
+
+				websocket2.close()
+				data1 = websocket1.receive_json()
+				print(data1)
+				websocket1.close()
+			except KeyboardInterrupt:
+				websocket2.close()
+				websocket1.close()
+
+
 def test_makeSuspicion_3players():
 	db.clear_tables()
 	host = get_random_string(6)
@@ -90,7 +174,7 @@ def test_makeSuspicion_3players():
 					print("Player 3 Cards: ", cards3)
 					print("Suspicion: ", victim, culprit, roomCard.cardId )
 
-					websocket3.send_json({'status': 'PICK_CARD', 'args': [victim]})
+					websocket3.send_json({'status': 'PICK_CARD', 'card': victim})
 					response = suspicion_post(player1.player_id, victim, culprit, client)
 					data1 = websocket1.receive_json()
 					data2 = websocket2.receive_json()
@@ -99,6 +183,12 @@ def test_makeSuspicion_3players():
 					print(data2)
 					print(data3)
 					data3 = websocket3.receive_json()
+					print(data3)
+					data1 = websocket1.receive_json()
+					data2 = websocket2.receive_json()
+					data3 = websocket3.receive_json()
+					print(data1)
+					print(data2)
 					print(data3)
 					data1 = websocket1.receive_json()
 					data2 = websocket2.receive_json()
