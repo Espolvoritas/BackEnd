a541517c3ce93ef3a4057c0dc9f42bf4349d9877
Valenteam24 <valen.arrascaeta@gmail.com>
2021-11-13 11:10:46 -0300

    Move enums to separate file

--- Misterio/database.py
+++ Misterio/database.py
@@ -5,56 +5,15 @@ from enum import Enum
 from pony.orm import flush
 from random import shuffle, choice
 
-from Misterio.board import makeBoard
+from Misterio.board import make_board
+from Misterio.enums import *
 
 db = Database()
-
-class ColorCode(Enum):
-    RED=1
-    GREEN=2
-    BLUE=3
-    WHITE=4
-    BLACK=5
-    YELLOW=6
-    PINK=7
-    ORANGE=8
 
 class Color(db.Entity):
     color_id = PrimaryKey(int, auto=True)
     colorName = Required(str)
     players = Set('Player', reverse='color')
-
-class CardValue(Enum):
-    pass
-
-class Monster(CardValue):
-    DRACULA = 1
-    FRANKENSTEIN = 2
-    WEREWOLF = 3
-    GHOST = 4
-    MUMMY = 5
-    JEKYLL = 6
-
-class Victim(CardValue):
-    COUNT = 7
-    COUNTESS = 8
-    HOUSEKEEPER = 9
-    BUTLER = 10
-    MAIDEN = 11
-    GARDENER = 12
-
-class Room(CardValue):
-    LOBBY = 13
-    LAB = 14
-    LIBRARY = 15
-    CELLAR = 16
-    ROOM = 17
-    PANTHEON = 18
-    HALL = 19
-    GARAGE = 20
-
-# class Salem(CardValue):
-#    SALEM = 21
 
 class Card(db.Entity):
     cardId = PrimaryKey(int, auto=True)
