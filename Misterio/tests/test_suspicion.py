73205ee5da6e8bcaec3794880ddf8b1151180ae3
Valenteam24 <valen.arrascaeta@gmail.com>
2021-11-16 17:30:29 -0300

    Test should pass every time regardless of the card distribution now

--- Misterio/tests/test_suspicion.py
+++ Misterio/tests/test_suspicion.py
@@ -1,11 +1,8 @@
 from fastapi.testclient import TestClient
 from pony.orm import db_session, flush, select
 from typing import Match
-import string
-import pytest
-import random # define the random module  
 from fastapi import WebSocketDisconnect
-from time import sleep
+from Misterio.functions import get_lobby_by_id
 from Misterio.testing_utils import *
 from Misterio.server import app
 import Misterio.database as db
@@ -80,33 +77,27 @@ def test_makeSuspicion_2players():
 			try:
 				print("Player 1 Cards: ", cards1)
 				print("Player 2 Cards: ", cards2)
-				
+				print("Suspicion: ", victim, monster, roomCard.card_id )
+
 				websocket2.send_json({'code': 'PICK_CARD', 'card': victim})
 				response = suspicion_post(player1.player_id, victim, monster, client)
-				
 				data2 = websocket2.receive_json()
+				print("Sus broadcast 2: ", data2)
 				
-				print(data2)
-
-				data2 = websocket2.receive_json()
-				print(data2)
-				
+				#Turn change
 				data1 = websocket1.receive_json()
 				data2 = websocket2.receive_json()
-				print(data1)
-				print(data2)
+				print("Turn change 1: ", data1)
+				print("Turn change 2: ", data2)
 
-				data1 = websocket1.receive_json()
 				data2 = websocket2.receive_json()
-				print(data1)
-				print(data2)
+				print("Player 2 pick card: ", data2)
 
 				assert (response.status_code == 200)
 				print(response)	
-
 				websocket2.close()
 				data1 = websocket1.receive_json()
-				print(data1)
+				print("Disconnect player 2: ", data1)
 				websocket1.close()
 			except KeyboardInterrupt:
 				websocket2.close()
@@ -117,47 +108,35 @@ def test_makeSuspicion_3players():
 	db.clear_tables()
 	host = get_random_string(6)
 	lobby_id = create_game_post(host, client).json()['lobby_id']
-	expectedPlayers = create_players(2, lobby_id)
-	expectedPlayers.insert(0,host)
-	
-	with client.websocket_connect("/lobby/1") as websocket1, \
-		client.websocket_connect("/lobby/2") as websocket2, \
-		client.websocket_connect("/lobby/3") as websocket3:
-		try:
-			data = websocket1.receive_json()
-			for player, expected in zip(data['players'], expectedPlayers):
-				assert (player['nickname'] == expected)
-			data = websocket2.receive_json()
-			for player, expected in zip(data['players'], expectedPlayers):
-				assert (player['nickname'] == expected)
-			data = websocket3.receive_json()
-			for player, expected in zip(data['players'], expectedPlayers):
-				assert (player['nickname'] == expected)
-
-			response = start_game_post(1, client)
-			assert (response.status_code == 200)
-			websocket3.close()
-			data = websocket1.receive_json()
-			data2 = websocket2.receive_json()
-			for player, expected in zip(data['players'], expectedPlayers):
-				assert (player['nickname'] == expected)
-			for player, expected in zip(data2['players'], expectedPlayers):
-				assert (player['nickname'] == expected)
-			websocket2.close()
-			data = websocket1.receive_json()
-			for player, expected in zip(data['players'], expectedPlayers):
-				assert (player['nickname'] == expected)
-			websocket1.close()
-		except KeyboardInterrupt:
-			websocket3.close()
-			websocket2.close()
-			websocket1.close()
 	
 	with db_session:
-		player1 = db.Lobby.get(lobby_id=lobby_id).game.current_player
+		lobby = get_lobby_by_id(lobby_id)
+		lobby.is_started = True
+		lobby.game = db.Game(lobby=lobby)
+		player2 = db.Player(nickname=get_random_string(6), alive=True)
+		player3 = db.Player(nickname=get_random_string(6), alive=True)
+		lobby.add_player(player2)
+		lobby.add_player(player3)
+		lobby.game.sort_players()
+		lobby.game.set_starting_positions()
+		player1 = lobby.game.current_player
 		player2 = player1.next_player
 		player3 = player2.next_player
+		lobby.game.fill_envelope()
 		
+
+		envelope = [lobby.game.monster, lobby.game.victim, lobby.game.room]
+		available_cards = list(select(c for c in db.Card if c not in envelope).order_by(db.Card.card_id))
+	
+		player = lobby.game.current_player
+		for c in available_cards:
+			c.assign(player)
+			player = player.next_player
+		
+		print(player1.cards)
+		print(player2.cards)
+		print(player3.cards)		
+
 		roomCards = select(c for c in player3.cards if c.is_room())
 		roomCard = roomCards.first()
 		room1 = db.Cell.get(room_name=roomCard.card_name)
@@ -169,6 +148,7 @@ def test_makeSuspicion_3players():
 		victim = select(c for c in db.Card if c in player3.cards and c.is_victim()).first()
 		monster = monster.card_id
 		victim = victim.card_id
+		
 	with client.websocket_connect("/gameBoard/" + str(player1.player_id)) as websocket1:
 		currplayer = websocket1.receive_json()
 		with client.websocket_connect("/gameBoard/" + str(player2.player_id)) as websocket2:
@@ -184,36 +164,40 @@ def test_makeSuspicion_3players():
 
 					websocket3.send_json({'code': 'PICK_CARD', 'card': victim})
 					response = suspicion_post(player1.player_id, victim, monster, client)
+					data2 = websocket2.receive_json()
+					print("Sus broadcast 2: ", data2)
+					data3 = websocket3.receive_json()
+					print("Sus broadcast 3: ",data3)
+					
+					#Turn change
 					data1 = websocket1.receive_json()
 					data2 = websocket2.receive_json()
 					data3 = websocket3.receive_json()
-					print(data1)
-					print(data2)
-					print(data3)
+					print("Turn change 1: ", data1)
+					print("Turn change 2: ", data2)
+					print("Turn change 3: ", data3)
+
 					data3 = websocket3.receive_json()
-					print(data3)
-					data1 = websocket1.receive_json()
+
+					print("False response 3: ", data3)
+
+					data3 = websocket3.receive_json()
+					print("Player3 pick card: ", data3)
+
 					data2 = websocket2.receive_json()
-					data3 = websocket3.receive_json()
-					print(data1)
-					print(data2)
-					print(data3)
-					data1 = websocket1.receive_json()
-					data2 = websocket2.receive_json()
-					data3 = websocket3.receive_json()
-					print(data1)
-					print(data2)
-					print(data3)
+					print("True response 2: ", data2)
+
 					assert (response.status_code == 200)
 					print(response)	
+
 					websocket3.close()
 					data1 = websocket1.receive_json()
 					data2 = websocket2.receive_json()
-					print(data1)
-					print(data2)
+					print("Disconnect player 3 (1): ", data1)
+					print("Disconnect player 3 (2): ", data2)
 					websocket2.close()
 					data1 = websocket1.receive_json()
-					print(data1)
+					print("Disconnect player 2: ", data1)
 					websocket1.close()
 				except KeyboardInterrupt:
 					websocket2.close()
