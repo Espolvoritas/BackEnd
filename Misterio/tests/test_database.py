ad8050fa3bb20b9b16d7cb63d9d223eee6fd412c
Valenteam24 <valen.arrascaeta@gmail.com>
2021-11-22 20:43:47 -0300

    Test stats using get method

--- Misterio/tests/test_database.py
+++ Misterio/tests/test_database.py
@@ -1,7 +1,12 @@
 from pony.orm import db_session, select, flush, commit
 from Misterio.functions import get_card_by_id
+from Misterio.testing_utils import get_stats
 from Misterio.tests import test_accuse, test_suspicion, test_move_player
 import Misterio.database as db
+from Misterio.server import app
+from fastapi.testclient import TestClient 
+
+client = TestClient(app)
 
 def test_player_creation():
     name = "test_player"
@@ -107,10 +112,45 @@ def test_stats():
         assert (won < gamestats.won_games)
         assert (traps < gamestats.trap_falls)
         print("Most chosen color: ", gamestats.most_chosen_color())
-        top_monster, top_victim, top_room = gamestats.envelope_top_cards()
+        (top_monster, percentage_m), (top_victim, percentage_v), (top_room, percentage_r) = gamestats.envelope_top_cards()
+        color_id, color_percent = gamestats.most_chosen_color() 
         assert (get_card_by_id(top_monster).is_monster())
         assert (get_card_by_id(top_victim).is_victim())
         assert (get_card_by_id(top_room).is_room())
+        print("Top monster: ", top_monster, "Top victim: ", top_victim, "Top room: ", top_room)
+        print("Average game time: ", gamestats.get_average_game_time())
+
+def test_get_stats():
+    response = get_stats(client)
+    assert (response.status_code == 200)
+    response_json = response.json()
+
+    with db_session:
+        gamestats = db.Stats.get(stats_id=1)
+        (top_monster, percentage_m), (top_victim, percentage_v), (top_room, percentage_r) = gamestats.envelope_top_cards()
+        color_id, color_percent = gamestats.most_chosen_color() 
+        assert (response_json["wrong_accusations"] == gamestats.wrong_accusations)
+        assert (response_json["right_accusations"] ==gamestats.right_accusations)
+        assert (response_json["suspicions_made"] ==gamestats.suspicions_made)
+        assert (response_json["won_games"] == gamestats.won_games)
+        assert (response_json["lost_games"] == gamestats.lost_games)
+        assert (response_json["trap_falls"] == gamestats.trap_falls)
+        assert (response_json["most_chosen_color"]["color_id"] == color_id)
+        assert (response_json["most_chosen_color"]["percentage"] == color_percent)
+        assert (response_json["top_envelope_monster"]["card_id"] == top_monster)
+        assert (response_json["top_envelope_monster"]["percentage"] == percentage_m)
+        assert (response_json["top_envelope_victim"]["card_id"] == top_victim)
+        assert (response_json["top_envelope_victim"]["percentage"] == percentage_v)
+        assert (response_json["top_envelope_room"]["card_id"] == top_room)
+        assert (response_json["top_envelope_room"]["percentage"] == percentage_r)
+        hours, minutes, seconds = gamestats.get_average_game_time()
+        assert (response_json["average_game_time"]["hours"] == hours)
+        assert (response_json["average_game_time"]["minutes"] == minutes)
+        assert (response_json["average_game_time"]["seconds"] == seconds)
+        print(top_monster, top_victim, top_room)
+        assert (get_card_by_id(int(top_monster)).is_monster())
+        assert (get_card_by_id(int(top_victim)).is_victim())
+        assert (get_card_by_id(int(top_room)).is_room())
         print("Top monster: ", top_monster, "Top victim: ", top_victim, "Top room: ", top_room)
         print("Average game time: ", gamestats.get_average_game_time())
 
